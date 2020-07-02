c5a23a628369a618ce4d3abaeffc4a518f8daefc
SimonDeLaus <36991248+SimonDeLaus@users.noreply.github.com>
2020-07-02 18:24:59 +0200

    Java-Kompendium - Uebung1

---
+++ Java/Java-Kompendium  - Professionell Java programmieren lernen/uebung1.java
@@ -0,0 +1,14 @@
+/*
+1.  Erstellen Sie ein Programm, das den Anwender zu unserem Java-Kurs begrüßt. Kompilieren Sie den Code und führen Sie ihn aus.
+2.  Fügen Sie in das Programm zwei Kommentare ein. Verwenden Sie hierfür unterschiedliche Auszeichnungsarten.
+*/
+
+// Neumann, Markus. Java: Kompendium: Professionell Java programmieren lernen (German Edition) (S.33-34). Kindle-Version.
+
+
+
+public class uebung1 {
+    public static void main(String[] args) {
+        System.out.println("Willkommen zu unserem Java-Kurs");
+    }
+}
