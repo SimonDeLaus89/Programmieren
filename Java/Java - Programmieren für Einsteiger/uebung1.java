edba0ecb700e1cff63f04396b52b7fb136ca98ae
SimonDeLaus <36991248+SimonDeLaus@users.noreply.github.com>
2020-07-02 18:16:23 +0200

    Java-Uebung1

---
+++ Java/Java - Programmieren für Einsteiger/uebung1.java
@@ -0,0 +1,17 @@
+/*
+1.  Schreiben Sie ein Programm, das mehrere Textzeilen auf dem Bildschirm ausgibt.
+    Um einen Zeilenumbruch zu erzeugen, ist es sinnvoll, anstatt System.out.print den Befehl System.out.println zu verwenden.
+    Auf diese Weise stehen die darauf folgenden Inhalte in einer neuen Zeile.
+2.  Fügen Sie in das eben erstellte Programm Kommentare ein, die das Verständnis erleichtern.
+
+Bonacina, Michael. Java Programmieren: für Einsteiger: Der leichte Weg zum Java-Experten (2. Auflage: komplett neu verfasst) (Einfach Programmieren lernen 1) (German Edition) (S.34). Kindle-Version.
+*/
+
+public class uebung1 {
+    public static void main(String[] args) {
+        System.out.println("Zeile 1");
+        System.out.println("Zeile 2");
+        System.out.println("Zeile 3");
+        System.out.println("Zeile 4");
+    }
+}
