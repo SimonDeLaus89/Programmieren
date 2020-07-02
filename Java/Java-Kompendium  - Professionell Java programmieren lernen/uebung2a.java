6b956813c1b33b9f6828c07976c1810f97abaf75
SimonDeLaus <36991248+SimonDeLaus@users.noreply.github.com>
2020-07-02 19:27:53 +0200

    Java-Kompendium - Uebung2

---
+++ Java/Java-Kompendium  - Professionell Java programmieren lernen/uebung2a.java
@@ -0,0 +1,24 @@
+/*
+1.  Erstellen Sie ein Programm, das drei Variablen für ganze Zahlen enthält.
+    Diese sollen jedoch jeweils einen unterschiedlichen Typ verwenden.
+    Geben sie einer von ihnen einen beliebigen Wert.
+    Weisen Sie daraufhin den beiden anderen Variablen den Wert der ersten Variablen zu.
+    Wählen Sie dafür Typen, die keine explizite Umformung des Variablentyps notwendig machen.
+
+Neumann, Markus. Java: Kompendium: Professionell Java programmieren lernen (German Edition) (S.60-61). Kindle-Version.
+*/
+
+public class uebung2a {
+    public static void main(String[] args) {
+        byte x = 1;
+        int y = 2;
+        long z = 3;
+
+        System.out.println("Werte: x: " + x + " y: " + y + " z: " + z);
+
+        y = x;
+        z = x;
+
+        System.out.println("Werte: x: " + x + " y: " + y + " z: " + z);
+    }
+}
---
+++ Java/Java-Kompendium  - Professionell Java programmieren lernen/uebung2b.java
@@ -0,0 +1,21 @@
+/*
+2.  Schreiben Sie ein Programm, das den Anwender zur Eingabe einer Zahl auffordert.
+    Das Programm soll daraufhin den doppelten Wert als Ergebnis ausgeben.
+
+Neumann, Markus. Java: Kompendium: Professionell Java programmieren lernen (German Edition) (S.60-61). Kindle-Version.
+*/
+
+import java.io.*;
+
+public class uebung2b {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        System.out.print("Geben Sie eine Zahl ein: ");
+        String x = br.readLine();
+
+        int zahl = Integer.parseInt(x);
+        System.out.println("Zahl * 2: " + zahl * 2);
+    }
+}
