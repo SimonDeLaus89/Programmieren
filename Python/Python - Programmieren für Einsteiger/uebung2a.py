6578e77515a3392ec382cf1a32cc8863a3a4eff5
SimonDeLaus <36991248+SimonDeLaus@users.noreply.github.com>
2020-07-03 22:40:35 +0200

    Python-Uebung2

---
+++ Python/Python - Programmieren für Einsteiger/uebung2a.py
@@ -0,0 +1,17 @@
+#
+# 1.    Schreiben Sie ein Programm, das den Anwender dazu auffordert, zwei Zahlen einzugeben.
+#       Speichern Sie diese in zwei unterschiedlichen Variablen.
+#       Das Programm soll danach die beiden Werte addieren und ausgeben.
+#
+# Bonacina, Michael. Python 3: Programmieren für Einsteiger: Der leichte Weg zum Python-Experten (Einfach Programmieren lernen 2) (German Edition) (S.54). Kindle-Version.
+#
+
+def main():
+    x = eval(input("Geben Sie eine erste Zahl ein: "))
+    y = eval(input("Geben Sie eine zweite Zahl ein: "))
+
+    z = x + y
+    print("Summe: {}".format(z))
+
+if __name__ == "__main__":
+    main()
---
+++ Python/Python - Programmieren für Einsteiger/uebung2b.py
@@ -0,0 +1,15 @@
+#
+# 2.    Schreiben Sie ein Programm, das den Anwender dazu auffordert, einen beliebigen Inhalt einzugeben.
+#       Speichern Sie den Wert in einer Variablen und geben Sie anschließend ihren Datentyp aus.
+#
+# Bonacina, Michael. Python 3: Programmieren für Einsteiger: Der leichte Weg zum Python-Experten (Einfach Programmieren lernen 2) (German Edition) (S.54-55). Kindle-Version.
+#
+
+
+def main():
+    x = eval(input("Geben Sie etwas ein: "))
+
+    print("Type: {}".format(type(x)))
+
+if __name__ == "__main__":
+    main()
