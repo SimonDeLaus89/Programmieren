1231627c88b78606291b0c444e49a5dc0019ef46
SimonDeLaus <36991248+SimonDeLaus@users.noreply.github.com>
2020-07-03 07:33:00 +0200

    Python-Uebung1

---
+++ Python/Python - Programmieren für Einsteiger/uebung1b.py
@@ -0,0 +1,14 @@
+#
+# 3.    Erstellen Sie ein neues Programm mit zwei print-Befehlen.
+#       Deren Inhalt soll jeweils 5 + 2 lauten – allerdings einmal in Anführungszeichen und einmal ohne Anführungszeichen.
+#       Führen Sie das Programm aus und überlegen Sie sich, worauf die unterschiedlichen Ausgaben zurückzuführen sind.
+#
+# Bonacina, Michael. Python 3: Programmieren für Einsteiger: Der leichte Weg zum Python-Experten (Einfach Programmieren lernen 2) (German Edition) (S.36-37). Kindle-Version.
+#
+
+def main():
+    print("5+2")
+    print (5+2)
+
+if __name__ == "__main__":
+    main()
