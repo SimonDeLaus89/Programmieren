3c39689602c2c465908144b70d05c4e4cf9f760c
SimonDeLaus <36991248+SimonDeLaus@users.noreply.github.com>
2020-07-05 20:42:25 +0200

    C#-Uebung2

---
+++ C#/C# - Programmieren für Einsteiger/uebung2a/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+1.  Erstellen Sie ein Programm, das jeweils eine int-, eine double und eine string-Variable enthält.
+    Multiplizieren Sie die Werte der int- und der double-Variablen und speichern Sie den Wert in einer neuen Variablen.
+    Wählen Sie hierfür einen passenden Typ.
+    Geben Sie anschließend das Ergebnis zusammen mit der string-Variablen aus.
+
+Bonacina, Michael. C# Programmieren: für Einsteiger: Der leichte Weg zum C#-Experten! (Visual Studio 2019) (Einfach Programmieren lernen 5) (German Edition) (S.52). Kindle-Version. 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uebung2a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            double y = 7.2;
+            double z = 0;
+            string a = "Moin";
+
+            z = x * y;
+
+            Console.WriteLine("String: {0}, Produkt: {1}", a, z);
+        }
+    }
+}
---
+++ C#/C# - Programmieren für Einsteiger/uebung2b/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+2.  Schreiben Sie ein Programm, das vom Anwender zwei Werte abfragt.
+    Berechnen Sie daraufhin das Ergebnis aus allen vier Grundrechenarten für die beiden Werte.
+    Geben Sie bei der Ausabe den entsprechenden Term und das Ergebnis an.
+    Das Programm soll es dem Anwender erlauben, Kommazahlen einzugeben.
+
+Bonacina, Michael. C# Programmieren: für Einsteiger: Der leichte Weg zum C#-Experten! (Visual Studio 2019) (Einfach Programmieren lernen 5) (German Edition) (S.52-53). Kindle-Version.
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uebung2b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0.0;
+            double y = 0.0;
+
+            Console.Write("Geben Sie die erste Zahl ein: ");
+            x = double.Parse(Console.ReadLine());
+
+            Console.Write("Geben Sie die zweite Zahl ein: ");
+            y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Summe: {0}", x + y);
+            Console.WriteLine("Differenz: {0}", x - y);
+            Console.WriteLine("Produkt: {0}", x * y);
+            Console.WriteLine("Quotient: {0}", x / y);
+        }
+    }
+}
