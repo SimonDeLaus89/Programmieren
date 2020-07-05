bbcadc72e796e2242d62d86a025efa3273064648
SimonDeLaus <36991248+SimonDeLaus@users.noreply.github.com>
2020-07-05 20:25:41 +0200

    C#-Uebung1

---
+++ C#/C# - Programmieren für Einsteiger/uebung1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+1.  Erstellen Sie ein Programm, das eine weitere Zeile zur Ausgabe hinzufügt, die auf den Inhalt des Kurses eingeht.
+2.  Fügen Sie auch für diese zweite Zeile einen passenden Kommentar ein.
+
+Bonacina, Michael. C# Programmieren: für Einsteiger: Der leichte Weg zum C#-Experten! (Visual Studio 2019) (Einfach Programmieren lernen 5) (German Edition) (S.33-34). Kindle-Version. 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uebung1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dieser Befehl gibt eine Begrüßung aus
+            Console.WriteLine("Willkommen zum C#-Kurs!");
+            //Diese Ausgabe geht auf die Inhalte ein.
+            Console.WriteLine("Hier lernen Sie, Programme mit C# zu erstellen.");
+        }
+    }
+}
