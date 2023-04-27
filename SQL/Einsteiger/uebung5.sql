1a987f8875ca3ae493679015af7f683a0f000f90
SimonDeLaus <simon.schmiedel@gmail.com>
2023-04-27 17:29:29 +0200

    SQL-Uebung5

---
+++ SQL/Einsteiger/uebung5.sql
@@ -0,0 +1,14 @@
+/*
+1.  Erstellen Sie einen neuen Account für die Datenbank mit dem Nutzernamen michaela_bauer und dem Passwort Ft4G6*s.
+2.  Weisen Sie diesem die Rechte zu, um die Struktur bestehender Tabellen zu verändern.
+    Suchen Sie hierfür in der oben verlinkten Tabelle nach einem passenden Ausdruck, der diese Berechtigung beinhaltet.
+    Diese Änderung soll sich nur auf die Tabelle mitglieder in der Datenbank vereinDB beziehen.
+3.  ErstellenSieEntziehen Sie dem Account alle Rechte für alle Datenbanken und alle Tabellen.
+4.  Löschen Sie den Account
+
+Fuchs, Paul. SQL: Handbuch für Einsteiger: Der leichte Weg zum SQL-Experten (Einfach Programmieren lernen 9) (German Edition) (S.85-86). BMU Media GmbH. Kindle-Version.
+*/
+
+USE OnlineshopXY;
+GO
+
